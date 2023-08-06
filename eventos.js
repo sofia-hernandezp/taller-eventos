a29f2c2820dd8d6b2fb0520d9d50a81c95d17e8d
Sofía Hernández <sofiachp95@gmail.com>
2023-08-06 03:31:09 -0300

    Nuevo js agregado

---
+++ eventos.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", () =>{
+
+    const unicoDiv = document.querySelector("div");
+
+        unicoDiv.addEventListener("click", () =>{
+        alert("¡Hola! Soy el div.");
+        });
+});
